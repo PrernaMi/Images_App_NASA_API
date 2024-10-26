48454c7ab5d40ab2f0a5cbb79fe92c4fc7ec5841
Prerna <prernamittal440@gmail.com>
2024-10-26 17:08:46 +0530

    Final Commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,8 +14,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
   bool? isLight;
   bool isFinished = false;
   MediaQueryData? mqData;
@@ -25,159 +23,28 @@ class _HomePageState extends State<HomePage> {
     context.read<MarsBloc>().add(GetImages());
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     isLight = Theme.of(context).brightness == Brightness.light;
     mqData = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: isLight! ? Colors.white : Colors.black,
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: mqData!.size.height * 0.02,
-          ),
-          Image.asset("assets/icons/space-travel.png",
-              height: 50,
-              fit: BoxFit.cover,
-              color: themeColor(isLight: isLight!)),
-          SizedBox(height: 7),
-          // SizedBox(height: mqData!.size.height*0.03,),
-          /*-------------Space Travel Font-----*/
-          RichText(
-            text: TextSpan(children: <TextSpan>[
-              TextSpan(
-                  text: "Space ",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: themeColor(isLight: isLight!),
-                      fontFamily: 'Light')),
-              TextSpan(
-                  text: "Travel",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: themeColor(isLight: isLight!),
-                      fontFamily: 'Dark'))
-            ]),
-          ),
-          SizedBox(height: 5),
-          Text("Trip To Mars"),
-          SizedBox(height: mqData!.size.height * 0.01),
-          /*--------Images ListView-----------*/
-          SizedBox(
-            height: mqData!.size.height * 0.47,
-            width: mqData!.size.width,
-            child: BlocBuilder<MarsBloc,MarsState>(
-              builder: (context,state) {
-                if(state is LoadingState){
-                  return Center(child: CircularProgressIndicator());
-                }
-                if(state is ErrorState){
-                  return Text(state.erroMsg);
-                }
-                if(state is LoadedState){
-                  return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: state.marsModel.photos!.length,
-                      shrinkWrap: true,
-                      itemBuilder: (_, index) {
-                        return Container(
-                          height: mqData!.size.height * 0.5,
-                          width: mqData!.size.width * 0.9,
-                          margin: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              state.marsModel.photos![index].imgSrc!,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        );
-                      });
-                }
-                return Container();
-              }
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          /*-------------Choose Spacecraft Font-----*/
-          RichText(
-            text: TextSpan(children: <TextSpan>[
-              TextSpan(
-                  text: "Choose ",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: themeColor(isLight: isLight!),
-                      fontFamily: 'Dark')),
-              TextSpan(
-                  text: "Spacecraft",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: themeColor(isLight: isLight!),
-                      fontFamily: 'Light'))
-            ]),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          /*-------------Choose Spacecraft Button-----*/
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              spaceCraftButton(
-                  color: Colors.grey.shade900,
-                  icon: Icons.rocket_launch,
-                  headText: "Orion X",
-                  text: "850 Mk/H"),
-              spaceCraftButton(
-                  color: Colors.green.shade800,
-                  icon: Icons.rocket_outlined,
-                  headText: "Dragon",
-                  text: "900 Mk/H"),
-              spaceCraftButton(
-                  color: Colors.grey.shade900,
-                  icon: Icons.rocket_launch_outlined,
-                  headText: "Alkay",
-                  text: "450 Mk/H"),
-            ],
-          ),
-          SizedBox(
-            height: 7,
-          ),
-          /*------Swipe Button----------*/
-          SizedBox(
-            width: mqData!.size.width * 0.6,
-            child: SwipeableButtonView(
-              onFinish: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SearchFlight();
-                }));
-                setState(() {
-                  isFinished = false; // Reset for future swipes
-                });
-              },
-              isFinished: isFinished,
-              onWaitingProcess: () async {
-                await Future.delayed(Duration(milliseconds: 2));
-                setState(() {
-                  isFinished = true; // Ensure state is updated immediately after
-                });
-              },
-              activeColor: Colors.grey.shade900,
-              buttonWidget: CircleAvatar(
-                backgroundColor: Colors.grey.shade800,
+      body: mqData!.orientation == Orientation.portrait
+          ? SingleChildScrollView(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [firstCol(), secondCol()],
               ),
-              buttonText: "Swipe Right",
+            )
+          : Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(flex: 1, child: firstCol()),
+                Expanded(flex: 1, child: secondCol())
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 
@@ -211,6 +78,188 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+    );
+  }
+
+  Widget firstCol() {
+    return Column(
+      mainAxisAlignment: mqData!.orientation == Orientation.landscape
+          ? MainAxisAlignment.center
+          : MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: mqData!.size.height * 0.02,
+        ),
+        Image.asset("assets/icons/space-travel.png",
+            height: 50,
+            fit: BoxFit.cover,
+            color: themeColor(isLight: isLight!)),
+        SizedBox(height: 7),
+        // SizedBox(height: mqData!.size.height*0.03,),
+        /*-------------Space Travel Font-----*/
+        RichText(
+          text: TextSpan(children: <TextSpan>[
+            TextSpan(
+                text: "Space ",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: themeColor(isLight: isLight!),
+                    fontFamily: 'Light')),
+            TextSpan(
+                text: "Travel",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: themeColor(isLight: isLight!),
+                    fontFamily: 'Dark'))
+          ]),
+        ),
+        SizedBox(height: 5),
+        Text("Trip To Mars"),
+        SizedBox(height: mqData!.size.height * 0.01),
+        /*--------Images ListView-----------*/
+        SizedBox(
+          height: mqData!.size.height * 0.47,
+          width: mqData!.size.width,
+          child: BlocBuilder<MarsBloc, MarsState>(builder: (context, state) {
+            if (state is LoadingState) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (state is ErrorState) {
+              return Text(state.erroMsg);
+            }
+            if (state is LoadedState) {
+              return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: state.marsModel.photos!.length,
+                  shrinkWrap: true,
+                  itemBuilder: (_, index) {
+                    return Expanded(
+                      child: Container(
+                        height: mqData!.size.height * 0.5,
+                        width: mqData!.size.width * 0.9,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            state.marsModel.photos![index].imgSrc!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            }
+            return Container();
+          }),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+      ],
+    );
+  }
+
+  Widget secondCol() {
+    return Column(
+      mainAxisAlignment: mqData!.orientation == Orientation.landscape
+          ? MainAxisAlignment.center
+          : MainAxisAlignment.start,
+      children: [
+        /*-------------Choose Spacecraft Font-----*/
+        RichText(
+          text: TextSpan(children: <TextSpan>[
+            TextSpan(
+                text: "Choose ",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: themeColor(isLight: isLight!),
+                    fontFamily: 'Dark')),
+            TextSpan(
+                text: "Spacecraft",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: themeColor(isLight: isLight!),
+                    fontFamily: 'Light'))
+          ]),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        /*-------------Choose Spacecraft Button-----*/
+        mqData!.orientation == Orientation.portrait
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  spaceCraftButton(
+                      color: Colors.grey.shade900,
+                      icon: Icons.rocket_launch,
+                      headText: "Orion X",
+                      text: "850 Mk/H"),
+                  spaceCraftButton(
+                      color: Colors.green.shade800,
+                      icon: Icons.rocket_outlined,
+                      headText: "Dragon",
+                      text: "900 Mk/H"),
+                  spaceCraftButton(
+                      color: Colors.grey.shade900,
+                      icon: Icons.rocket_launch_outlined,
+                      headText: "Alkay",
+                      text: "450 Mk/H"),
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  spaceCraftButton(
+                      color: Colors.grey.shade900,
+                      icon: Icons.rocket_launch,
+                      headText: "Orion X",
+                      text: "850 Mk/H"),
+                  spaceCraftButton(
+                      color: Colors.green.shade800,
+                      icon: Icons.rocket_outlined,
+                      headText: "Dragon",
+                      text: "900 Mk/H"),
+                  spaceCraftButton(
+                      color: Colors.grey.shade900,
+                      icon: Icons.rocket_launch_outlined,
+                      headText: "Alkay",
+                      text: "450 Mk/H"),
+                ],
+              ),
+        SizedBox(
+          height: 7,
+        ),
+        /*------Swipe Button----------*/
+        SizedBox(
+          width: mqData!.size.width * 0.6,
+          child: SwipeableButtonView(
+            onFinish: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return SearchFlight();
+              }));
+              setState(() {
+                isFinished = false; // Reset for future swipes
+              });
+            },
+            isFinished: isFinished,
+            onWaitingProcess: () async {
+              await Future.delayed(Duration(milliseconds: 2));
+              setState(() {
+                isFinished = true; // Ensure state is updated immediately after
+              });
+            },
+            activeColor: Colors.grey.shade900,
+            buttonWidget: CircleAvatar(
+              backgroundColor: Colors.grey.shade800,
+            ),
+            buttonText: "Swipe Right",
+          ),
+        )
+      ],
     );
   }
 }
